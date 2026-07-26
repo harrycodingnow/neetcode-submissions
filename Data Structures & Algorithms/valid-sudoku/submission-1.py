5c31fba15e32aecef38c7dc123167429751db3d9
Harry <harrygoodwork@gmail.com>
2026-07-26 10:46:45 +0800

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(len(board)):
+            row_set = set()
+            for j in range(len(board[0])):
+                if board[i][j] == ".":
+                    continue
+                elif board[i][j] in row_set:
+                    return False
+                else:
+                    row_set.add(board[i][j])
+        
+        
+        for x in range(len(board[0])):
+            column_set = set()
+            for y in range(len(board)):
+                if board[y][x] == ".":
+                    continue
+                elif board[y][x] in column_set: 
+                    return False
+                else:
+                    column_set.add(board[y][x])
+
+        top_left = [
+            (0, 0),
+            (0, 3),
+            (0, 6),
+            (3, 0),
+            (3, 3),
+            (3, 6),
+            (6, 0),
+            (6, 3),
+            (6, 6),
+        ]
+
+        
+        for a, b in top_left:
+            square = set()
+            for da in range(0,3):                
+                for db in range(0,3):
+                    if board[a + da][b + db] == ".":
+                        continue
+                    elif board[a + da][b + db] in square:
+                        return False
+                    else:
+                        square.add(board[a+da][b+db])
+            
+        return True
+
+
+        
